6f01b02122248ee82274b82ba78ec354c23d5cfc
96berry96 <miltoncsedev@gmail.com>
2024-04-18 10:11:26 +0100

    rxjs of and from operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { from } from 'rxjs';
+import { of } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +11,39 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular-observables';
 
+  complete:any = false;
   data:any [] = [];
+
+  array1 = [1,3,5,7,9];
+  array2 = ['A', 'B', 'C', 'D'];
+
+  myObservable = from(this.array1);
+
+  GetAsyncData(){
+    // this.myObservable.subscribe((data: any) => {
+    //   this.data.push(data)
+    // },
+    // (error: any) => {
+    //   alert(error.message)
+    // },
+    // () => {
+    //   alert('All the data is streamed')
+    // })
+    this.myObservable.subscribe({
+      next: (data: any) => {
+        this.data.push(data)
+      },
+      error(err){
+        alert(err.message)
+      },
+      complete:() =>{
+        this.complete = true
+        setTimeout(()=> {
+          this.complete = false
+        }, 9000)
+        
+        
+      }
+    })
+  }
 }
